0e2a4ab66c4b702f1567991f98382b4a3f8b6a2d
sonugupta <sonugupta27032001@gmail.com>
2026-06-23 15:02:44 +0530

    this is me

--- budget-management-backend/test.py
+++ budget-management-backend/test.py
@@ -1,12 +1,14 @@
 from google import genai
 
-client = genai.Client(
-    api_key="AQ.Ab8RN6K94nGyGtaFv58qzskQwa3j_YADkA2hX0QybFZ0xvYS1g"
-)
+API_KEY = "AQ.Ab8RN6I2RRsg4rRZkEvURL0Q3zoDXiGr-o2s4Di35FtkNb1uKw"
+
+print("KEY LENGTH:", len(API_KEY))
+
+client = genai.Client(api_key=API_KEY)
 
 response = client.models.generate_content(
     model="gemini-2.5-flash",
-    contents="hello"
+    contents="Hello"
 )
 
 print(response.text)
